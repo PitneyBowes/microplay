8efa688e7dcaf92a3aed5dcf219a9c9a940bed6c
yaron.yamin <yaron.yamin@borderfree.com>
2018-01-01 14:37:20 +0200

    import play micro logic from listing-service. remove IDEA generated boilerplate code

---
+++ app/com.borderfree/micro/play/controllers/components/ErrorHandler.scala
@@ -0,0 +1,32 @@
+package com.borderfree.micro.play.controllers.components
+
+import com.borderfree.micro.play.logging.LogSupport
+import play.api.libs.json.Writes
+import play.api.mvc.{RequestHeader, Result, Results}
+
+/**
+  * Created with IntelliJ IDEA.
+  * User: yaron.yamin
+  * Date: 1/26/2016
+  * Time: 9:25 AM
+  */
+
+
+trait ErrorHandler[ERR]{
+
+  this: Results with HttpContentNegotiator with LogSupport =>
+
+  implicit val errWriter: Writes[ERR]
+
+  def mapExceptionToErrorResponse(err: Throwable): (Status, ERR)
+
+  def renderErrorResponse(err: Throwable)(implicit request: RequestHeader): Result =
+  {
+    logger.error("handling error", err)
+    val tuple: (Status, ERR) = mapExceptionToErrorResponse(err)
+    tuple match
+    {
+      case (httpStatus, errorResponse) => renderResult(httpStatus, errorResponse)
+    }
+  }
+}
