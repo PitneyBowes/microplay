3e4f2c31f9c97bb13adbb820f87bda52cb39c61e
yaron.yamin <yaron.yamin@borderfree.com>
2018-01-01 15:08:47 +0200

    fix health controller integration test

--- it/com/borderfree/micro/play/controllers/HealthCheckControllerTest.scala
+++ it/com/borderfree/micro/play/controllers/HealthCheckControllerTest.scala
@@ -6,18 +6,19 @@ import play.api.test._
 class HealthCheckControllerTest extends PlayFakeAppSpecification{
 
   "HealthCheckController" should {
-    "health end point" in new WithApplication(getFakeApp) {
+//    "health end point" in new WithApplication(getFakeApp) {
+    "health end point" in new WithApplication {
       val Some(result) = route(app, FakeRequest(GET, "/health"))
       status(result) mustEqual OK
       contentAsString(result) mustEqual "I'm Ok. Thanks for checking."
     }
 
-    "env end point" in new WithApplication(getFakeApp) {
+    "env end point" in new WithApplication {
       val Some(result) = route(app, FakeRequest(GET, "/env"))
       status(result) mustEqual OK
-      contentAsString(result) mustEqual "this is test env"
+      contentAsString(result) mustEqual "default env"
     }
-    "build info status end point" in new WithApplication(getFakeApp) {
+    "build info status end point" in new WithApplication {
       val Some(result) = route(app, FakeRequest(GET, "/status"))
       status(result) mustEqual OK
       contentAsString(result) must contain("\"builtAtMillis\"")
