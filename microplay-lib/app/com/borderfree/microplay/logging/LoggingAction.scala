db3d63130d25137be428e02e823c7d35f86b0293
yaron.yamin <yaron.yamin@borderfree.com>
2018-11-06 09:42:52 +0200

    http trace: do not trace response body for black listed request uri specified in configuration key micro.trace.response-body.exclude-uris consisting of a comma delimited list of request uris. no_rally_id

--- microplay-lib/app/com/borderfree/microplay/logging/LoggingAction.scala
+++ microplay-lib/app/com/borderfree/microplay/logging/LoggingAction.scala
@@ -20,6 +20,7 @@ class LoggingAction @Inject()(appConfiguration: AppConfiguration,parser: BodyPar
   val REQUEST_BODY = "REQUEST_BODY"
   val RESPONSE_BODY = "RESPONSE_BODY"
   lazy val RequestedCorrelationIdHeaderName: String = appConfiguration.getString("micro.correlation.requested.header-name")
+  lazy val ExlucdedUrisForResponseBodyTracing: Set[String] = appConfiguration.getString("micro.trace.response-body.exclude-uris").split(",").toSet
 
   val FormatMsg: (String, String, String) => String = (action: String, actionType: String, data: String)=>{
     s"action=$action actionType=$actionType data=$data"
@@ -27,28 +28,8 @@ class LoggingAction @Inject()(appConfiguration: AppConfiguration,parser: BodyPar
 
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] =
   {
-
-    def retrieveCharset(result: Result): String =
-    {
-      val header = result.header.headers.getOrElse("Content-Type", "")
-      if (header != "" && header.contains("; charset="))
-      {
-        header.substring(header.indexOf("; charset=") + 10, header.length()).trim()
-      }
-      else
-      {
-        "UTF-8"
-      }
-    }
-
-    def logAction(actionType: String, data: String) =
-    {
-      if(!request.uri.endsWith( "health")) {
-        logger.info(FormatMsg(request.uri, actionType, data))
-      }
-    }
     mdcManager.putCorrelationId(request.headers.get(RequestedCorrelationIdHeaderName)) //todo add a configuration option to ignore received correlationId and set a fresh one instead ( might be useful in external apis)
-    logAction(REQUEST_BODY,
+    logAction(request,REQUEST_BODY,
       {
         request.body match
         {
@@ -80,8 +61,10 @@ class LoggingAction @Inject()(appConfiguration: AppConfiguration,parser: BodyPar
           result.body.consumeData(mat) onComplete
             {
               case Success(bytesString) =>
-                val body = bytesString.decodeString(retrieveCharset(result))
-                logAction(actionType = RESPONSE_BODY, data =  if (logger.isDebugEnabled) body else "")
+                if (logger.isDebugEnabled && !ExlucdedUrisForResponseBodyTracing.contains(request.uri)){
+                  val body = bytesString.decodeString(retrieveCharset(result))
+                  logAction(request,actionType = RESPONSE_BODY, data =  body )
+                }
               case Failure(e) =>
                 logger.error("error occurred:", e)
             }
@@ -91,4 +74,24 @@ class LoggingAction @Inject()(appConfiguration: AppConfiguration,parser: BodyPar
     mdcManager.clear()
     resultWithCorrelationIdHeader
   }
+
+  private def logAction[A](request: Request[A], actionType: String, data: String) =
+  {
+    if(!request.uri.endsWith( "health")) {
+      logger.debug(FormatMsg(request.uri, actionType, data))
+    }
+  }
+  private def retrieveCharset(result: Result): String =
+  {
+    val header = result.header.headers.getOrElse("Content-Type", "")
+    if (header != "" && header.contains("; charset="))
+    {
+      header.substring(header.indexOf("; charset=") + 10, header.length()).trim()
+    }
+    else
+    {
+      "UTF-8"
+    }
+  }
+
 }
